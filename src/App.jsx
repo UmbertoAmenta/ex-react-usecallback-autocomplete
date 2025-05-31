189868ef0fd9c32493c2eda7bfd6a2057c1b9930
Umberto Amenta <umberto.amenta@hotmail.it>
2025-05-31 16:21:52 +0200

    feat: milestone 2 (implementare il debounce per ottimizzare la ricerca)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import SearchBar from "./componentes/SearchBar";
 import Suggestions from "./componentes/Suggestions";
@@ -7,15 +7,34 @@ export default function App() {
   const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
+  function debounce(call, delay) {
+    let timer;
+    return (value) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        call(value);
+      }, delay);
+    };
+  }
+
+  const fetchProducts = useCallback(
+    debounce(async (search) => {
+      if (search === "") {
+        setProducts([]);
+        return;
+      }
+
       const request = await fetch(
         `http://localhost:5000/products?search=${search}`
       );
       const data = await request.json();
       setProducts(data);
-    };
-    fetchProducts();
+    }, 300),
+    []
+  );
+
+  useEffect(() => {
+    fetchProducts(search);
   }, [search]);
 
   return (
